ba01caf710204473c6fa4de8240b93c017ef5d3f
Nicholas Sutter <gastlyTree@gmail.com>
2017-01-06 12:58:33 -0800

    Started coding

---
+++ intro.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+//Similar to "Using" statements in C# or "Import" in Java
+//Tells the pre-processor that there is some existing code that we want to utilize in this code.
+//Note that the <> brackets tell the pre-processor to look in the built in standard libraries.
+//We will use "" instead of <> to tell the pre-processor to look in the local project first.
+
+//Note that the pre-processor reads the current source file looking for pre-processor directives (start with #).
+//In this case, the #include tells the pre-processor to find stdio.h and substitute its text into this file.
+
+
+//Another type of pre-processor directive is the "define" statement.
+//Note that the define statement does not end with a semi-colon.
+#define PI 3.14159
+#define Radius 2
+
+
+void PrimitiveDataTypes()
+{
+	char c = 'R';
+	int x = 80;
+	float f = PI;
+	double d = 12345.6597;
+	//Print the value of variables
+	//Note the use of the format specifiers that start with %
+	printf("The value of c is %c\n", c);
+	printf("The value of c as an int %d\n", c);
+	printf("The value of x is %d\n", x);
+	printf("The value of x as a character %c\n", x);
+	printf("The value of f is %f\n", f);
+	//use format specifiers to modify output. For example, to display 10 digits total, with two decimals:
+	printf("The value of d is %10.2f\n", d);
+
+	//What are the sizes of each data type
+	//The sizeof() function is used to return the size of a datatype, struct, or an array, etc.
+	printf("The size of char is %d\n", sizeof(c));
+	printf("The size of an int is %d\n", sizeof(x));
+	printf("The size of a float is %d\n", sizeof(float));
+	printf("The size of a double is %d\n", sizeof(d));
+
+}
+
+//Modifiers tot he primitive data types
+void Modifiers()
+{
+	//note that c defaults to signed values
+
+	//unsigned --> positive values only
+	unsigned char e = 255;
+	signed char c = 127;
+
+	//short and long
+	short int x;
+	short y;
+	long int z;
+	
+
+	//add one to e and c
+	e = e + 1;
+	c = c + 1;	
+
+	printf("The value of e is %d\n",e);	
+	printf("The value of c is %d\n",c);
+
+	//display the size of short and long modifiers. Note that a short is 2 bytes inthis environment
+	printf("The size of x and y are %d and %d respectively.\n", sizeof(x), sizeof(y));
+	printf("the size of z id %d.\n", sizeof(z));
+
+}
+
+void DemoConstants()
+{
+	//note the keyword const
+	const float pi = 3.14159;
+	printf("The value of pi id %f\n", pi);
+}
+
+void main (void)
+{
+	//Use printf to output to the console.
+	//printf("Hello cruel world\n");
+
+	//PrimitiveDataTypes();
+	Modifiers();
+	DemoConstants();
+}
