dc25d6b05bc1b9de51e7b9b3ad6a0e70912a3d6f
Nicholas Sutter <gastlyTree@gmail.com>
2017-02-14 14:45:05 -0600

    started on assignment 1

---
+++ CST204ASSIGN1/program.c
@@ -0,0 +1,18 @@
+/*
+    Course: COSC292
+    Name:   Nicholas Sutter
+    CST#:   CST204
+    Assgn:  1
+    Description:
+        Used to test and run the entire program for assignment 1.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "question1.h"
+
+int main()
+{
+
+    return EXIT_SUCCESS;    
+}
---
+++ CST204ASSIGN1/question1.h
@@ -0,0 +1,16 @@
+/*
+    Course: COSC292
+    Name:   Nicholas Sutter
+    CST#:   CST204
+    Assgn:  1
+    Description:
+       Header file for question 1, assignment 1. describes the stringCompare
+       function. 
+*/
+
+#ifndef QUESTION1_H_
+#define QUESTION1_H_
+
+int stringCompare (const char * string1, const char * string2);
+
+#endif
