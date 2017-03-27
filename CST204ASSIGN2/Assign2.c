35e771766f82dfa9bd34bbe1c7addf1e844590cd
Nicholas Sutter <gastlyTree@gmail.com>
2017-03-27 14:10:14 -0600

    Continued to work on the assignment

--- CST204ASSIGN2/Assign2.c
+++ CST204ASSIGN2/Assign2.c
@@ -18,13 +18,27 @@ Vehicle * CreateVehicle (char * cVinP, char * cMakeP, char * cModelP)
 {
     Vehicle * vPtr = (Vehicle *)malloc(sizeof(Vehicle));
     
-    vPtr->cVin = 
+    vPtr->cVin = cVinP;
+    vPtr->cMake = cMakeP;
+    vPtr->cModel = cModelP;
+    vPtr->repairs = 
+    vPtr->iNumRepairs = 0;
+
+    return vPtr;
 }
 
 //Create a repair with the information passed in.  Return a pointer to the repair.
 Repair * CreateRepair (int iRepairID, double dCost, char * cDescriptonP, char * dateP)
 {
-    
+    Repair * rPtr = (Repair *)malloc(sizeof(Repair));
+
+    rPtr->iRepairID = iRepairID;
+    rPtr->dCost = dCost;
+    rPtr->cDescripton = (char *)malloc((strlen(cDescriptonP)+1)*sizeof(char));
+    strcpy(rPtr->cDescripton, cDescriptonP);
+    rPtr->date = dateP;
+
+    return rPtr;
 }
 
 //Given a vehicle and a pointer to a repair, add this repair directly to the vehicle
@@ -37,7 +51,17 @@ void AddRepair(Vehicle * vP, Repair * rP)
 //Display the data regarding the vehicle an all of its repairs
 void DisplayVehicle(Vehicle v)
 {
+    printf("Vin Number: %s\n", v.cVin);
+    printf("Make: %s\t\tModel: %s\n", v.cMake, v.cModel);
+    printf("Repairs\n");
     
+    for(int var = 0; var < v.iNumRepairs; ++var)
+    {
+        printf("\tRepair Id: %d\n", v.iNumRepairs[var].iRepairID);
+        printf("\tCost: %f\n", v.iNumRepairs[var].dCost);
+        printf("\tDescription: %s\n", v.iNumRepairs[var].cDescripton);
+        printf("\tDate: %s\n\n", v.iNumRepairs[var].date);
+    }
 }
 
 //This will locate a repair with the given id.  If the repair is not found,
@@ -52,4 +76,16 @@ Repair * FindRepair( Vehicle v, int id)
 void DeleteRepair(int RepairID, Vehicle * v)
 {
     
+}
+
+//free a given vehicle and all of its repairs
+void freeVehicle(char * vPtr)
+{
+    for(int var = 0; var < vPtr->iNumRepairs; ++var)
+    {
+        free(vPtr->repairs[var]->cDescripton);
+        vPtr->repairs[var]->cDescripton = NULL;
+    }
+    free(vPtr);
+    vPtr = NULL;
 }
